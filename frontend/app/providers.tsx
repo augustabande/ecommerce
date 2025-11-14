51ef8e9285b265d05d93ec81c30d6a03eb3cd90c
bandeau <augusta.bande@ext.ec.europa.eu>
2025-11-14 16:31:20 +0100

    fix: fixed providers

--- frontend/app/providers.tsx
+++ frontend/app/providers.tsx
@@ -1,19 +1,18 @@
 'use client'
 
-import { QueryClient } from 'react-query'
+import { QueryClient } from '@tanstack/react-query'
 import { MedusaProvider } from 'medusa-react'
-import { useState } from 'react'
 
 export function Providers({ children }: { children: React.ReactNode }) {
- const queryClient = new QueryClient()
+  const queryClient = new QueryClient()
 
   return (
     <MedusaProvider
-    queryClientProviderProps={{ client: queryClient }}
-    baseUrl={process.env.NEXT_PUBLIC_MEDUSA_BACKEND_URL || 'http://localhost:9000'}
-    publishableApiKey={process.env.NEXT_PUBLIC_MEDUSA_PUBLISHABLE_KEY}
-  >
-    {children}
-  </MedusaProvider>
+      queryClientProviderProps={{ client: queryClient }}
+      baseUrl={process.env.NEXT_PUBLIC_MEDUSA_BACKEND_URL || 'http://localhost:9000'}
+      publishableApiKey={process.env.NEXT_PUBLIC_MEDUSA_PUBLISHABLE_KEY}
+    >
+      {children}
+    </MedusaProvider>
   )
 }
